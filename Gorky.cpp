027e6b518ba57bb631e0b43bde776596f85ea557
Sergey Ushakov <softogen@gmail.com>
2023-06-03 12:40:16 +0300

    Initial automatically generated version.

---
+++ Gorky.cpp
@@ -0,0 +1,12 @@
+﻿// Gorky.cpp : Defines the entry point for the application.
+//
+
+#include "Gorky.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello CMake." << endl;
+    return 0;
+}
